d82dbc7da6faa1f918ae9af51e815145c3a747d7
JaeBeen95 <nin2348@gmail.com>
2025-06-24 18:29:17 +0900

    ♻️ refactor: MovieList페이지 뷰, 로직 분리

--- src/features/movie/components/MovieListView.tsx
+++ src/features/movie/components/MovieListView.tsx
@@ -1,38 +1,11 @@
 import MovieCard from '@/features/movie/components/MovieCard';
-import MovieSkeletonCard from '@/features/movie/components/MovieCardSkeleton';
 import type { Movie } from '@/features/movie/types/movie';
 
 interface MovieListViewProps {
-  status: 'idle' | 'loading' | 'error' | 'success';
   movieList: Movie[];
 }
 
-export default function MovieListView({ status, movieList }: MovieListViewProps) {
-  if (status === 'loading')
-    return (
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
-        {Array.from({ length: 10 }).map((_, i) => (
-          <MovieSkeletonCard key={i} />
-        ))}
-      </div>
-    );
-
-  if (status === 'error') {
-    return (
-      <div className="text-red-400 text-center py-10">
-        <p>영화 데이터를 불러오지 못했습니다.</p>
-      </div>
-    );
-  }
-
-  if (movieList.length === 0) {
-    return (
-      <div className="text-center py-10">
-        <p>영화가 없습니다.</p>
-      </div>
-    );
-  }
-
+export default function MovieListView({ movieList }: MovieListViewProps) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
       {movieList.map((movie) => (
--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -1,15 +1,46 @@
-import MovieListView from '@/features/movie/components/MovieListView';
+import MovieSkeletonCard from '@/features/movie/components/MovieCardSkeleton';
 import MovieListLayout from '@/features/movie/components/MovieListLayout';
+import MovieListView from '@/features/movie/components/MovieListView';
 import { usePopularMovies } from '@/features/movie/hooks/useMovies';
 
 export default function MovieList() {
   const { data: movieList, isLoading, isError } = usePopularMovies({ page: 1 });
 
-  const status = isLoading ? 'loading' : isError ? 'error' : 'success';
+  if (isLoading) {
+    return (
+      <MovieListLayout>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
+          {Array.from({ length: 10 }).map((_, i) => (
+            <MovieSkeletonCard key={i} />
+          ))}
+        </div>
+      </MovieListLayout>
+    );
+  }
+
+  if (isError) {
+    return (
+      <MovieListLayout>
+        <div className="text-red-400 text-center py-10">
+          <p>영화 데이터를 불러오지 못했습니다.</p>
+        </div>
+      </MovieListLayout>
+    );
+  }
+
+  if (!movieList || movieList.results.length === 0) {
+    return (
+      <MovieListLayout>
+        <div className="text-center py-10">
+          <p>영화가 없습니다.</p>
+        </div>
+      </MovieListLayout>
+    );
+  }
 
   return (
     <MovieListLayout>
-      <MovieListView status={status} movieList={movieList?.results || []} />
+      <MovieListView movieList={movieList.results} />
     </MovieListLayout>
   );
 }
